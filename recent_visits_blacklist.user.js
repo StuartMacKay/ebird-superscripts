b744bef275e15416bbeb78d01d2c3cb1f3cf6118
Stuart MacKay <smackay@flagstonesoftware.com>
2018-03-26 15:32:08 +0100

    Improved the Recent Visits page blacklist.
    Rewrote the script so all the checklists from any observer can be
    hidden by clicking on a checkmark next to their name. The checklist
    are only hidden until the page is loaded.
    
    Updated the list of URLs the script work for to include hotspots.
    Previously it only worked for regions (contrary to the descirption).

--- recent_visits_blacklist.user.js
+++ recent_visits_blacklist.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name     Recent visits observer blacklist
 // @namespace https://github.com/ProjectBabbler/ebird/
-// @version  1.0.0
+// @version  2.0.0
 // @grant    none
 // @include  https://ebird.org/region/*/activity*
+// @include  https://ebird.org/hotspot/*/activity*
 // @require  https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js
-// @description Hide checklists from certain observers on the recent visits page for a region or hotspot
+// @description Hide checklists from selected observers on the recent visits page for a region or hotspot
 // @copyright 2018 Stuart MacKay (https://github.com/ProjectBabbler/ebird-superscripts)
 // @homepage https://github.com/ProjectBabbler/ebird-superscripts
 // @author smackay
@@ -16,16 +17,23 @@
 (function() {
     'use strict';
 
-    // Add the names of the observers you want to exclude from the recent
-    // visits page for a hotspot or region.
+    var name;
 
-    const blacklist = ['Stuart MacKay']; // just an example ;)
+    function hideObserver(name) {
+        $('td[headers="observer"]').each(function () {
+            if (name ===  $(this).data('observer')) {
+                $(this).closest('tr').addClass('hidden');
+            }
+        });
+    }
 
     $('td[headers="observer"]').each(function () {
-        let name = $(this).contents().text().trim().replace(/\s{2,}/, ' ');
-        if (blacklist.includes(name)) {
-            $(this).parent().addClass('hidden');
-        }
+        name = $(this).contents().text().trim().replace(/\s{2,}/, ' ');
+        $(this).prepend('<span style="font-weight: normal;" title="Hide all checklists from' + name + '">x</span> ');
+        $(this).data('observer', name);
+        $(this).children(":first").click(function () {
+            hideObserver(name);
+        });
     });
 
 })();
